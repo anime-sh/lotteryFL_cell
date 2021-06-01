3cbce0251cb84dc07d86b6fdf04e6b00f18e08e7
Parth Jindal <pmjindal@gmail.com>
2021-06-01 11:07:16 +0530

    add testing utils

--- dataset/cifar10_noniid.py
+++ dataset/cifar10_noniid.py
@@ -14,9 +14,9 @@ def get_dataset_cifar10_extr_noniid(num_users, n_class, nsamples, rate_unbalance
                                     transform=apply_transform)
 
     # Chose equal splits for every user
-    user_groups_train, user_groups_test = cifar_extr_noniid(
+    user_groups_train, user_groups_test, class_idxs = cifar_extr_noniid(
         train_dataset, test_dataset, num_users, n_class, nsamples, rate_unbalance)
-    return train_dataset, test_dataset, user_groups_train, user_groups_test
+    return train_dataset, test_dataset, user_groups_train, user_groups_test, class_idxs
 
 
 def cifar_extr_noniid(train_dataset, test_dataset, num_users, n_class, num_samples, rate_unbalance):
@@ -53,11 +53,12 @@ def cifar_extr_noniid(train_dataset, test_dataset, num_users, n_class, num_sampl
         idxs_test_splits[labels_test[i]].append(idxs_test[i])
 
     idx_shards = np.split(idx_shard, 10)
-
+    class_idxs = []
     # divide and assign
     for i in range(num_users):
         user_labels = np.array([])
         temp_set = set(np.random.choice(10, n_class, replace=False))
+        class_idxs.append(temp_set)
         rand_set = []
         for j in temp_set:
             choice = np.random.choice(idx_shards[j], 1)[0]
@@ -82,4 +83,4 @@ def cifar_extr_noniid(train_dataset, test_dataset, num_users, n_class, num_sampl
         for label in user_labels_set:
             dict_users_test[i] = np.concatenate(
                 (dict_users_test[i], idxs_test_splits[int(label)]), axis=0)
-    return dict_users_train, dict_users_test
+    return dict_users_train, dict_users_test, class_idxs
--- dataset/datasource.py
+++ dataset/datasource.py
@@ -83,7 +83,7 @@ def iid_split(num_clients,
 def get_data_noniid_cifar10(num_users, n_class, nsamples, batch_size=32, rate_unbalance=1.0, num_workers=1):
 
     train_data, test_data = [], []
-    train_data, test_data, user_train, user_test = get_dataset_cifar10_extr_noniid(
+    train_data, test_data, user_train, user_test, class_idxs = get_dataset_cifar10_extr_noniid(
         num_users, n_class, nsamples, rate_unbalance)
 
     train_loaders = []
@@ -108,7 +108,7 @@ def get_data_noniid_cifar10(num_users, n_class, nsamples, batch_size=32, rate_un
             test_data, batch_sampler=sampler_test, num_workers=num_workers)
         test_loaders.append(loader_test)
 
-    return train_loaders, test_loaders
+    return train_loaders, test_loaders, class_idxs
 
 
 def get_data_noniid_mnist(num_users, n_class, nsamples, batch_size=32, rate_unbalance=1.0, num_workers=1):
